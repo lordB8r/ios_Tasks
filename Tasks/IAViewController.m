165dff7f69f84d9d9a9c0124aef82a9cada3028e
lordB8r <jp@zavteq.com>
2013-10-22 08:38:49 -0500

    reviewed and cleaned up

--- Tasks/IAViewController.m
+++ Tasks/IAViewController.m
@@ -25,48 +25,45 @@
     self.tasks = [[NSMutableArray alloc] init];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return [self.tasks count];
 }
 
-- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if(cell == nil)
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier ];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                        reuseIdentifier:CellIdentifier ];
     cell.textLabel.text = self.tasks[indexPath.row];
     
     return cell;
 }
 
-- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [self performSegueWithIdentifier:@"taskSegue" sender:self.tasks[indexPath.row ]];
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self performSegueWithIdentifier:@"taskSegue"
+          sender:self.tasks[indexPath.row ]];
 }
 
-- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     UIViewController *destination = segue.destinationViewController;
     
     if ([segue.identifier isEqualToString:@"taskSegue"])
-        [destination setValue:sender forKeyPath:@"task"];
+        [destination setValue:sender
+                     forKeyPath:@"task"];
     else
         destination = [segue.destinationViewController topViewController];
     
-    [destination setValue:self forKeyPath:@"delegate"];
-         
+    [destination setValue:self
+                 forKeyPath:@"delegate"];
 }
 
--(void) viewWillAppear:(BOOL)animated
-{
+-(void) viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self.tableView reloadData];
 }
